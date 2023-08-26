cbd9fb7b7bd56fc1de53d7d520ec213e563a51e5
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-08-26 18:35:18 -0300

    feat: calculating the highest in-degree vertex and its predecessors

--- Main.java
+++ Main.java
@@ -33,7 +33,7 @@ public class Main{
             vertexDegree[line] = count;
         }
 
-        //check wich vertex has the most edges coming out of it
+        //checking wich vertex has the most edges coming out of it
         int max = 0;
         int vertex = 1;
         for(int i = 1; i < totalVertex; i++){
@@ -43,34 +43,101 @@ public class Main{
             }
         }
 
-        //print the results
+        //printing the results
         System.out.println("Vértice com maior grau de saída - (" + vertex + "), de grau " + max);
 
         return vertex;
     }
     
-    public static ArrayList<Integer> successors(int vertex, int totalVertex, int totalEdges, int[][] matrix){
+    public static void successors(int vertex, int totalVertex, int totalEdges, int[][] matrix){
         ArrayList<Integer> edges = new ArrayList<>();
 
+        //loop through all values ​​(columns) of the vertex passed by parameter
         for(int col = 1; col < totalEdges; col++){
+            //if the value is '-1', that matrix column is an edge of that vertex
             if(matrix[vertex][col] == -1){
+                //saving every edge of the vertex 
                 edges.add(col);
             }
         }
 
         ArrayList<Integer> successors = new ArrayList<>();
+        //loop through the edges of the vertex
         for(int i : edges){
+            //loop through all rows (vertex)
             for(int line = 1; line < totalVertex; line++){
+                //if the row has the value '1' with the column it is a successor
                 if(matrix[line][i] == 1){
+                    //saving every successor
                     successors.add(line);
                     break;
                 }
             }
         }
 
-        System.out.println("Lista de sucessores - " + successors.toString());
+        //printing the results
+        System.out.println("Lista de sucessores do vértice (" + vertex + ") - " + successors.toString());
+    }
 
-        return successors;
+    public static int maxInDegree(int totalVertex, int totalEdges, int[][] matrix){
+        //array where each position representes one vertex
+        int[] vertexDegree = new int[totalVertex];
+
+        //loop through all values ​​(columns) of each row (vertex)
+        for(int line = 1; line < totalVertex; line++){
+            int count = 0;
+            for(int col = 1; col < totalEdges; col++){
+                //checking how many edges are coming in the vertex (line)
+                if(matrix[line][col] == 1) count++;
+            }
+            //saving the amount of edges that are coming in this vertex (line)
+            vertexDegree[line] = count;
+        }
+
+        //checking wich vertex has the most edges coming in it
+        int max = 0;
+        int vertex = 1;
+        for(int i = 1; i < totalVertex; i++){
+            if(vertexDegree[i] > max) {
+                max = vertexDegree[i];
+                vertex = i;
+            }
+        }
+
+        //printing the results
+        System.out.println("Vértice com maior grau de entrada - (" + vertex + "), de grau " + max);
+
+        return vertex;
+    }
+
+    public static void predecessors(int vertex, int totalVertex, int totalEdges, int[][] matrix){
+        ArrayList<Integer> edges = new ArrayList<>();
+
+        //loop through all values ​​(columns) of the vertex passed by parameter
+        for(int col = 1; col < totalEdges; col++){
+            //if the value is '1', that matrix column is an edge of that vertex
+            if(matrix[vertex][col] == 1){
+                //saving every edge of the vertex 
+                edges.add(col);
+            }
+        }
+
+        ArrayList<Integer> predecessors = new ArrayList<>();
+        //loop through the edges of the vertex
+        for(int i : edges){
+            //loop through all rows (vertex)
+            for(int line = 1; line < totalVertex; line++){
+                //if the row has the value '1' with the column it is a successor
+                if(matrix[line][i] == -1){
+                    //saving every successor
+                    predecessors.add(line);
+                    break;
+                }
+            }
+        }
+
+        //printing the results
+        System.out.println("Lista de predecessores do vértice (" + vertex + ") - " + predecessors.toString());
     }
 
     public static void main(String[] args) throws Exception {
@@ -95,19 +162,12 @@ public class Main{
             int[][] matrix = new int[totalVertex][totalEdges];
 
             buildMatrix(sc, matrix);
-            /*
-            for(int line = 1; line < totalVertex; line++){
-                for(int col = 1; col < totalEdges; col++){
-                    System.out.print(matrix[line][col] + " _ ");
-                }
-                System.out.println();
-            }
-            */
 
             int maxOutDegreeVertex = maxOutDegree(totalVertex, totalEdges, matrix);
             successors(maxOutDegreeVertex, totalVertex, totalEdges, matrix);
-
-            System.err.println("=============================================================================================================");
+            System.out.println("============================================================================================================================");
+            int maxInDegreeVertex = maxInDegree(totalVertex, totalEdges, matrix);
+            predecessors(maxInDegreeVertex, totalVertex, totalEdges, matrix);
 
             sc.close();
         } else{
